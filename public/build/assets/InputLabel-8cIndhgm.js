75e6cc4c556d74864c62d8a18b90b8c2cd6b6ed9
dpsghapps-coder <dpsghapps@gmail.com>
2026-04-02 06:40:15 +0000

    Add build folder

---
+++ public/build/assets/InputLabel-8cIndhgm.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-CwaS30Gf.js";function n({value:t,className:e,children:r,...s}){return a.jsx("label",{...s,className:`text-sm font-medium text-gray-700 ${e}`,children:t||r})}export{n as I};
